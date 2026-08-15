d915f718f4733f538d8fb6c9f7ff251fbaf62b13
norin168 <channorin19@gmail.com>
2026-08-15 09:47:19 +0700

    missing mockup data

--- src/mock/buy-stock.ts
+++ src/mock/buy-stock.ts
@@ -4,7 +4,6 @@ export const buyStockRecords: BuyStockRecord[] = [
   {
     id: 1,
     type: "buy",
-    goldType: "buy",
     weight: 1000,
     purify: 9999,
     price: 9000,
@@ -14,7 +13,6 @@ export const buyStockRecords: BuyStockRecord[] = [
   {
     id: 2,
     type: "buy",
-    goldType: "existing",
     weight: 500,
     purify: 50,
     price: 2250,
@@ -26,6 +24,8 @@ export const buyStockRecords: BuyStockRecord[] = [
     type: "stock",
     weight: 200,
     purify: 9999,
+    price: 1800,
+    currency: "USD",
     date: "2026-07-25",
   },
 ];
